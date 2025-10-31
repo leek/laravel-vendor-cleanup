0d7465bac609f4ef76ae317d29f58f2bf3cb5b58
Chris Jones <leeked@gmail.com>
2025-10-31 00:29:27 -0400

    chore: adding misc package files

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude([
+        'bootstrap/cache',
+        'node_modules',
+        'storage',
+        'vendor',
+    ])
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+$config = new PhpCsFixer\Config;
+
+return $config
+    ->setRules([
+        '@PSR12' => true,
+        '@PhpCsFixer' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => [
+            'default' => 'single_space',
+            'operators' => ['=>' => null],
+        ],
+        'blank_line_after_opening_tag' => true,
+        'braces' => [
+            'allow_single_line_anonymous_class_with_empty_body' => true,
+        ],
+        'compact_nullable_typehint' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'declare_equal_normalize' => true,
+        'function_typehint_space' => true,
+        'new_with_braces' => true,
+        'method_argument_space' => [
+            'on_multiline' => 'ensure_fully_multiline',
+            'keep_multiple_spaces_after_comma' => true,
+        ],
+        'no_empty_statement' => true,
+        'no_leading_import_slash' => true,
+        'no_leading_namespace_whitespace' => true,
+        'no_multiline_whitespace_around_double_arrow' => true,
+        'multiline_whitespace_before_semicolons' => false,
+        'no_singleline_whitespace_before_semicolons' => true,
+        'no_unused_imports' => true,
+        'not_operator_with_successor_space' => true,
+        'object_operator_without_whitespace' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'phpdoc_indent' => true,
+        'phpdoc_inline_tag_normalizer' => true,
+        'phpdoc_no_access' => true,
+        'phpdoc_no_package' => true,
+        'phpdoc_no_useless_inheritdoc' => true,
+        'phpdoc_scalar' => true,
+        'phpdoc_single_line_var_spacing' => true,
+        'phpdoc_summary' => true,
+        'phpdoc_to_comment' => true,
+        'phpdoc_trim' => true,
+        'phpdoc_types' => true,
+        'phpdoc_var_without_name' => true,
+        'self_accessor' => true,
+        'single_quote' => true,
+        'standardize_not_equals' => true,
+        'ternary_operator_spaces' => true,
+        'trim_array_spaces' => true,
+        'whitespace_after_comma_in_array' => true,
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ->setUsingCache(true);
